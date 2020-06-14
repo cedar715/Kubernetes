e734327c879f61635eb10195af2850a5995bb834
cedar715 <v.rao.jai@gmail.com>
2020-06-14 20:41:08 +0530

    CKAD examples.

---
+++ projects/note-taking-app/src/main/java/learnk8s/io/knoteapp/KnoteAppApplication.java
@@ -0,0 +1,13 @@
+package learnk8s.io.knoteapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KnoteAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KnoteAppApplication.class, args);
+	}
+
+}
---
+++ projects/note-taking-app/src/test/java/learnk8s/io/knoteapp/KnoteAppApplicationTests.java
@@ -0,0 +1,13 @@
+package learnk8s.io.knoteapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KnoteAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
